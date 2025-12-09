3e99515582cc55d810030cd4942740454113247d
Aabhisek63 <aabhisekpal59@gmail.com>
2025-12-09 22:43:48 +0000

    Day 2

---
+++ basics/day2.js
@@ -0,0 +1,23 @@
+let a =23;
+const b="abc";
+var c= 78;
+d=88;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// let a=11;
+// const b=45;
+// var c=74;
+// d=25;
+
+let e="abcd";
+let f="";
+let g=null;
+let h;
+
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
