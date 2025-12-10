9c8a0e778404a8f77152032f19b5e024eeb232bd
Aabhisek63 <aabhisekpal59@gmail.com>
2025-12-10 05:21:01 +0000

    Day3

---
+++ basics/third.js
@@ -0,0 +1,31 @@
+// // 
+
+// let fruits=["apple","banana","mango","guava","grapes"];
+ 
+// for(let i=1;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+
+// fruits.push("Papaya");
+// fruits.unshift("Kiwi");
+// fruits.includes("mango");
+// fruits.indexOf("Banana");
+
+// for(let value of fruits){
+//     console.log(value);
+// }
+
+
+let numbers=[2,4,6,8];
+ let num2=numbers.map(x=>x*2);
+
+  for(let value of num2){
+     console.log(value);
+ }
+
+
+ let num=[10,25,30,5,60];
+ let num3=num.filter(x=>x>2);
+ for(let value of num3){
+     console.log(value);
+ }
